9cbb8861ad955fa4281c14259238412c09bdb6e0
Kingsley Ijomah <git-example@codehance>
2022-10-02 18:47:16 -0400

    User controller

--- src/app.js
+++ src/app.js
@@ -1,4 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express({type: 'application/vnd.api+json'}));
+app.use(express.urlencoded({extended: true}));
+
+// routes
+const userRoutes = require('./routes/user.routes');
+app.use('/api/', userRoutes);
+
 
 module.exports = app;
