9f65dd0380818d46a42756049ddddc83aa8090e9
Kingsley Ijomah <git-example@codehance>
2022-10-02 17:28:43 -0400

    Adding user services

---
+++ src/services/user.services.js
@@ -0,0 +1,16 @@
+const db = require('../config/database');
+
+// create a new user
+const createUser = async (body) => {
+  const { first_name, last_name, email, password, confirm_password } = body;
+  const { rows } = await db.query(
+    'INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING *',
+    [first_name, last_name, email, password],
+  );
+  return rows[0];
+}
+
+// export all the functions
+module.exports = {
+  createUser,
+};
