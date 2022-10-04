25f46721db8487ad018f548e360564b91aafed6d
Kingsley Ijomah <git-example@codehance>
2022-10-04 12:13:39 -0400

    Add login controller and routing

--- src/app.js
+++ src/app.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const cors = require('cors');
+const cookieSession = require('cookie-session');
 const app = express();
 
 // middlewares
 app.use(cors());
 app.use(express.json());
-app.use(express({type: 'application/vnd.api+json'}));
-app.use(express.urlencoded({extended: true}));
+app.use(express({ type: 'application/vnd.api+json' }));
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    name: process.env.COOKIE_NAME,
+    secret: process.env.COOKIE_SECRET,
+    httpOnly: true,
+    sameSite: 'strict',
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  })
+);
 
 // routes
 const userRoutes = require('./routes/user.routes');
-app.use('/api/', userRoutes);
+const authRoutes = require('./routes/auth.routes');
 
+app.use('/api/', userRoutes);
+app.use('/api/', authRoutes);
 
 module.exports = app;
