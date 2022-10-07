b4eb2bec353094b3206dfc2f4d7a50e969c4dcfd
Kingsley Ijomah <git-example@codehance>
2022-10-07 09:20:53 -0400

    Adding job service

---
+++ src/services/job.services.js
@@ -0,0 +1,47 @@
+const db = require('../config/database');
+
+// create a new job
+const createJob = async (body) => {
+  const { title, salary_type, salary, job_types, skills, description, company_id, sector_id, category_id } = body;
+
+  const { rows } = await db.query(
+    'INSERT INTO jobs (title, salary_type, salary, job_types, skills, description, company_id, sector_id, category_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *',
+    [title, salary_type, salary, job_types, skills, description, company_id, sector_id, category_id],
+  );
+
+  return rows[0];
+}
+
+// get all jobs
+const getAllJobs = async () => {
+  const { rows } = await db.query('SELECT * FROM jobs');
+
+  return rows;
+}
+
+// update a job
+const updateJob = async (id, body) => {
+  const { title, salary_type, salary, job_types, skills, description, company_id, sector_id, category_id } = body;
+
+  const { rows } = await db.query(
+    'UPDATE jobs SET title = $1, salary_type = $2, salary = $3, job_types = $4, skills = $5, description = $6, company_id = $7, sector_id = $8, category_id = $9 WHERE id = $10 RETURNING *',
+    [title, salary_type, salary, job_types, skills, description, company_id, sector_id, category_id, id],
+  );
+
+  return rows[0];
+}
+
+// delete a job
+const deleteJob = async (id) => {
+  const { rows } = await db.query('DELETE FROM jobs WHERE id = $1 RETURNING *', [id]);
+
+  return rows[0];
+}
+
+// export all functions
+module.exports = {
+  createJob,
+  getAllJobs,
+  updateJob,
+  deleteJob,
+};
