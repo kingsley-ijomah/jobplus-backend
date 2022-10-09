7096af84e0e3bd0f7120ae39c8ca86aa871a28ac
Kingsley Ijomah <git-example@codehance>
2022-10-09 11:22:51 -0400

    using joins pt2

--- src/services/user_job.services.js
+++ src/services/user_job.services.js
@@ -41,9 +41,20 @@ const getUserJobsByUserAndType = async (body) => {
     `
     SELECT
       jobs.*,
-      json_agg(companies) AS company
+      json_agg(DISTINCT companies) AS company,
+      json_agg(DISTINCT sectors) AS sector,
+      json_agg(DISTINCT skills) AS skills,
+      json_agg(DISTINCT saved) AS user_saved,
+      json_agg(DISTINCT notification) AS user_notification,
+      json_agg(DISTINCT application) AS user_application
     FROM jobs
-    LEFT JOIN companies ON jobs.company_id = companies.id 
+      LEFT JOIN companies ON jobs.company_id = companies.id
+      LEFT JOIN sectors ON jobs.sector_id = sectors.id
+      LEFT JOIN job_skills ON jobs.id = job_skills.job_id
+      LEFT JOIN skills ON job_skills.skill_id = skills.id
+      LEFT JOIN user_jobs AS saved ON jobs.id = saved.job_id AND saved.type = 'Saved' AND saved.user_id = $1
+      LEFT JOIN user_jobs AS notification ON jobs.id = notification.job_id AND notification.type = 'Notification' AND notification.user_id = $1
+      LEFT JOIN user_jobs AS application ON jobs.id = application.job_id AND application.type = 'Application' AND application.user_id = $1
     WHERE jobs.id IN (
       SELECT job_id
       FROM user_jobs
