fa33b26675b0efa3a444eb1e162a02e935193c66
Kingsley Ijomah <git-example@codehance>
2022-10-11 08:40:35 -0400

    Adding job search service

--- src/services/job.services.js
+++ src/services/job.services.js
@@ -42,6 +42,56 @@ const deleteJob = async (id) => {
   return rows[0];
 }
 
+// search jobs
+const searchJobs = async (params) => {
+  const { user_id, limit=10, page=1, what, where } = params;
+
+  const offset = (page - 1) * limit;
+
+  const { rows } = await db.query(
+    `
+    SELECT
+      jobs.*,
+      json_agg(DISTINCT companies) AS company,
+      json_agg(DISTINCT sectors) AS sector,
+      json_agg(DISTINCT skills) AS skills,
+      json_agg(DISTINCT saved) AS user_saved,
+      json_agg(DISTINCT notification) AS user_notification,
+      json_agg(DISTINCT application) AS user_application
+    FROM jobs
+      LEFT JOIN companies ON jobs.company_id = companies.id
+      LEFT JOIN sectors ON jobs.sector_id = sectors.id
+      LEFT JOIN job_skills ON jobs.id = job_skills.job_id
+      LEFT JOIN skills ON job_skills.skill_id = skills.id
+      LEFT JOIN user_jobs AS saved ON jobs.id = saved.job_id AND saved.type = 'Saved' AND saved.user_id = $1
+      LEFT JOIN user_jobs AS notification ON jobs.id = notification.job_id AND notification.type = 'Notification' AND notification.user_id = $1
+      LEFT JOIN user_jobs AS application ON jobs.id = application.job_id AND application.type = 'Application' AND application.user_id = $1
+    WHERE 
+    (
+      jobs.title ILIKE $4
+      OR companies.name ILIKE $4
+      OR jobs.id IN (
+        SELECT job_id FROM job_skills WHERE skill_id IN (
+          SELECT id FROM skills WHERE name ILIKE $4
+        )
+      )
+    )
+    AND
+    (
+      companies.city ILIKE $5
+      OR companies.district ILIKE $5
+    )
+    GROUP BY jobs.id
+    ORDER BY jobs.created_at DESC
+    LIMIT $3
+    OFFSET $4
+    `,
+    [user_id, limit, offset, what, where]
+  );
+
+  return rows;
+}
+
 // export all functions
 module.exports = {
   createJob,
